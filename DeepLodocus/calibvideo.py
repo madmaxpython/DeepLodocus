1dd002fad334d7c1d5c8bb70b482b9624fe76682
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2022-08-09 10:40:51 -0400

    Minor update

--- DeepLodocus/calibvideo.py
+++ DeepLodocus/calibvideo.py
@@ -5,44 +5,46 @@ Created on Mon Apr 25 16:40:02 2022
 
 @author: maximeteixeira
 """
-import os, glob
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from matplotlib.widgets import PolygonSelector
+import glob
 import json
-from pathlib import Path
+import os
 import random
-import cv2
+from pathlib import Path
 
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib.widgets import PolygonSelector
 
 SCRIPT_PATH = str(Path(__file__).parent)
 
-with open(SCRIPT_PATH+"/config.txt", "r") as config_file:
+with open(SCRIPT_PATH + "/config.txt", "r") as config_file:
     config = json.loads(config_file.read())
-    
-os.chdir(SCRIPT_PATH+"/Datas")
 
-VIDEO_TO_TREAT=[]
+os.chdir(SCRIPT_PATH + "/Datas")
+
+VIDEO_TO_TREAT = []
 for file in glob.glob("*.mp4"):
     VIDEO_TO_TREAT.append(file)
 
-def AreaSelector(zone_nb,cage,config, element):
+
+def AreaSelector(zone_nb, cage, config, element):
     fig, ax = plt.subplots()
     ax.invert_yaxis()
     fig.canvas.manager.set_window_title('Calibration Step')
-    fig.suptitle('Please, define Zone '+str(config['zone_name'][zone_nb])+' for cage'+str(cage), fontsize=16)
-    
-    cap = cv2.VideoCapture(SCRIPT_PATH+"/Datas/"+element)
-    randomnb=random.randint(0,300)
+    fig.suptitle('Please, define Zone ' + str(config['zone_name'][zone_nb]) + ' for cage' + str(cage), fontsize=16)
+
+    cap = cv2.VideoCapture(SCRIPT_PATH + "/Datas/" + element)
+    randomnb = random.randint(0, 300)
     cap.set(1, randomnb)
-    
+
     ret, frame = cap.read()
     imgplot = plt.imshow(frame)
-    
+
     lineprops = {'color': 'red', 'linewidth': 4, 'alpha': 0.8}
     lsso = PolygonSelector(ax=ax, onselect=onSelect, lineprops=lineprops)
     plt.show()
     return coord
+
 
 def onSelect(x):
     global coord
@@ -51,26 +53,27 @@ def onSelect(x):
     coord = x
     return coord
 
-def zone_file(config, VIDEO_TO_TREAT):
-    areas_list={}
 
-    for cage in range (1,20):
-        current_cage=[]
+def zone_file(config, VIDEO_TO_TREAT):
+    areas_list = {}
+
+    for cage in range(1, 20):
+        current_cage = []
         for element in VIDEO_TO_TREAT:
-            if element[0]==str(cage):
+            if element[0] == str(cage):
                 current_cage.append(element)
-        
-        if current_cage!=[]:
+
+        if current_cage:
             for calibration in range(len(config['zone_name'])):
-                areas_list["Cage"+str(cage)+"_"+"Zone"+str(config['zone_name'][calibration])]=AreaSelector(calibration,cage,config, current_cage[0])
+                areas_list["Cage" + str(cage) + "_" + "Zone" + str(config['zone_name'][calibration])] = AreaSelector(
+                    calibration, cage, config, current_cage[0])
 
     areas_list = json.dumps(areas_list)
 
-    zone_file = open(SCRIPT_PATH+"/zone.txt", "w")
+    zone_file = open(SCRIPT_PATH + "/zone.txt", "w")
     zone_file.write(str(areas_list))
     zone_file.close()
 
+
 if __name__ == "__main__":
     zone_file(config, VIDEO_TO_TREAT)
-
-
--- DeepLodocus/main.py
+++ DeepLodocus/main.py
@@ -16,98 +16,90 @@ from pathlib import Path
 
 script_path = str(Path(__file__).parent)
 
-# Import config dictionnary from a .txt file
-with open(script_path+'/config.txt', "r") as config_file:
+# Import config dictionary from a .txt file
+with open(script_path + '/config.txt', "r") as config_file:
     config = json.loads(config_file.read())
 
 
-class Mouse ():
-    num_of_mice=0
-    
+class Mouse():
+    num_of_mice = 0
+
     def __init__(self, csvname):
         self.csvname = csvname
-        self.cage=csvname[0]
+        self.cage = csvname[0]
         self.name = csvname.split('_')[1].split('.')[0]
-        self.data = pd.read_csv(script_path+'/Datas/'+self.csvname,header=[1,2,3], index_col=0)
-        self.likelihood = np.array(self.data.loc[:, [x for x in self.data.columns.values if 'likelihood'  in str(x)]])
-        #Create a dataframe without likelihood columns
+        self.data = pd.read_csv(script_path + '/Datas/' + self.csvname, header=[1, 2, 3], index_col=0)
+        self.likelihood = np.array(self.data.loc[:, [x for x in self.data.columns.values if 'likelihood' in str(x)]])
+        # Create a dataframe without likelihood columns
         columns_to_use = [x for x in self.data.columns.values if 'likelihood' not in str(x)]
-        self.data_reduced = self.data.loc[:,columns_to_use]  
-        
-        Mouse.num_of_mice+=1
-        
+        self.data_reduced = self.data.loc[:, columns_to_use]
+
+        Mouse.num_of_mice += 1
+
     def analyze_zone(self):
-        with open(script_path+"/zone.txt", "r") as zone_file:
+        with open(script_path + "/zone.txt", "r") as zone_file:
             areas = json.loads(zone_file.read())
 
         for zone in areas:
-            areas[zone]=Polygon(areas[zone])
-        
-        
+            areas[zone] = Polygon(areas[zone])
+
         output_zone = analysis.TotalTime(config, areas, self.data_reduced, self.likelihood, self.cage)
-        
-        self.time_zone=output_zone['time_zone']
+
+        self.time_zone = output_zone['time_zone']
         self.entries_zone = output_zone['entries_zone']
-    
-        
+
     def analyze_distance(self):
-        self.distance_travelled=analysis.TotalDistance(config, self.data_reduced, self.likelihood)
-        
+        self.distance_travelled = analysis.TotalDistance(config, self.data_reduced, self.likelihood)
+
+
 if __name__ == "__main__":
-    df_analysis=pd.DataFrame()
+    df_analysis = pd.DataFrame()
 
-    #Look for csv files, and create an instance Mouse for each of them
-    for file in glob.glob(os.path.join(script_path,'Datas', "*.csv")):
-        mouse_id = glob.glob(os.path.join(script_path,'Datas', "*.csv")).index(file)
-        globals()[f"mouse_{mouse_id}"]=Mouse(file.split("Datas/",1)[1])
-        df_analysis[globals()[f"mouse_{mouse_id}"].name]=0
-        
+    # Look for csv files, and create an instance Mouse for each of them
+    for file in glob.glob(os.path.join(script_path, 'Datas', "*.csv")):
+        mouse_id = glob.glob(os.path.join(script_path, 'Datas', "*.csv")).index(file)
+        globals()[f"mouse_{mouse_id}"] = Mouse(file.split("Datas/", 1)[1])
+        df_analysis[globals()[f"mouse_{mouse_id}"].name] = 0
 
+    # If time or entries in zones need to be analyze, create a list for each of them
+    if config['analyse_zone'] == True or config['analyse_entries'] == True:
+        for zone in config['zone_name']:
+            globals()[f"list_time_{zone}"] = []
+            globals()[f"list_entries_{zone}"] = []
 
-    #If time or entries in zones need to be analyze, create a list for each of them
-    if config['analyse_zone']==True or config['analyse_entries']==True:
-        for zonee in config['zone_name']:
-            globals()[f"list_time_{zonee}"]= []
-            globals()[f"list_entries_{zonee}"]= []
-     
-    #If distance travelled need to be analyze, create a list        
-    if config['analyse_distance']==True:
-         list_distance=[]
+    # If distance travelled need to be analyzed, create a list
+    if config['analyse_distance']:
+        list_distance = []
 
-    #For each measure, fill a list of this measure for all the mice 
-    for mice_nb in range (Mouse.num_of_mice):    
-        if config['analyse_zone']==True or config['analyse_entries']==True:
-        
+    # For each measure, fill a list with this measure for all the mice
+    for mice_nb in range(Mouse.num_of_mice):
+        if config['analyse_zone'] == True or config['analyse_entries'] == True:
+
             globals()[f"mouse_{mice_nb}"].analyze_zone()
-        
+
             for zone in config['zone_name']:
-            
-                if config['analyse_zone']==True:
+
+                if config['analyse_zone']:
                     globals()[f"list_time_{zone}"].append(globals()[f"mouse_{mice_nb}"].time_zone[zone])
 
-                if config['analyse_entries']==True:
+                if config['analyse_entries']:
                     globals()[f"list_entries_{zone}"].append(globals()[f"mouse_{mice_nb}"].entries_zone[zone])
 
-                if mice_nb == Mouse.num_of_mice-1: #If it is the last mouse, create a row in dataframe made of the list of this measure
-                    if config['analyse_zone']==True:
-                        df_analysis.loc['Time in zone '+zone+' (sec)']=globals()[f"list_time_{zone}"]
-                    
-                    if config['analyse_entries']==True: #If it is the last mouse, create a row in dataframe made of the list of this measure
-                        df_analysis.loc['Entries in '+zone]=globals()[f"list_entries_{zone}"]
 
-                
-        if config['analyse_distance']==True:
+                    if config['analyse_zone']:
+                        df_analysis.loc['Time in zone ' + zone + ' (sec)'] = globals()[f"list_time_{zone}"]
+
+                    if config['analyse_entries']:  # If it is the last mouse, create a row in dataframe made of the list of this measure
+                        df_analysis.loc['Entries in ' + zone] = globals()[f"list_entries_{zone}"]
+
+        if config['analyse_distance']:
             globals()[f"mouse_{mice_nb}"].analyze_distance()
             list_distance.append(globals()[f"mouse_{mice_nb}"].distance_travelled)
-        
-            if mice_nb == Mouse.num_of_mice-1:
-                df_analysis.loc['Distance travelled (cm)']=list_distance
-        
-        
-    FILE_NAME=str(sys.argv[1])
+
+            if mice_nb == Mouse.num_of_mice - 1:
+                df_analysis.loc['Distance travelled (cm)'] = list_distance
+
+    FILE_NAME = str(sys.argv[1])
+    print(FILE_NAME)
     print('\n \nData CPP Video __________________________________________________\n', df_analysis)
-    df_analysis.to_excel(script_path +'/Output/'+FILE_NAME+'.xlsx')
-
-
-        
-    
+    df_analysis.to_excel(script_path + '/Output/' + FILE_NAME + '.xlsx')
