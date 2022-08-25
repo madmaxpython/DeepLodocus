3d5f0bdebde49e19264340349b704b2c13e5ff44
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2022-08-24 20:41:03 -0400

    Import AreaSelector form utils.py

--- DeepLodocus/calibvideo.py
+++ DeepLodocus/calibvideo.py
@@ -5,72 +5,46 @@ Created on Mon Apr 25 16:40:02 2022
 
 @author: maximeteixeira
 """
-import os, glob
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from matplotlib.widgets import PolygonSelector
+import glob
 import json
-from pathlib import Path
-import random
-import cv2
+import os
 
+from pathlib import Path
+from utils import AreaSelector
 
 SCRIPT_PATH = str(Path(__file__).parent)
 
-with open(SCRIPT_PATH+"/config.txt", "r") as config_file:
+with open(SCRIPT_PATH + "/config.txt", "r") as config_file:
     config = json.loads(config_file.read())
-    
-os.chdir(SCRIPT_PATH+"/Datas")
 
-VIDEO_TO_TREAT=[]
+os.chdir(SCRIPT_PATH + "/Datas")
+
+VIDEO_TO_TREAT = []
 for file in glob.glob("*.mp4"):
     VIDEO_TO_TREAT.append(file)
-    
-def AreaSelector(zone_nb,cage,config, element):
-    fig, ax = plt.subplots()
-    ax.invert_yaxis()
-    fig.canvas.manager.set_window_title('Calibration Step')
-    fig.suptitle('Please, define Zone '+str(config['zone_name'][zone_nb])+' for cage'+str(cage), fontsize=16)
-    
-    cap = cv2.VideoCapture("/Users/maximeteixeira/Desktop/DeepLodocus/Datas/"+element)
-    randomnb=random.randint(0,30000)
-    cap.set(1, randomnb)
-    
-    ret, frame = cap.read()
-    imgplot = plt.imshow(frame)
-    
-    lineprops = {'color': 'red', 'linewidth': 4, 'alpha': 0.8}
-    lsso = PolygonSelector(ax=ax, onselect=onSelect, lineprops=lineprops)
-    plt.show()
-    return coord
 
-def onSelect(x):
-    global coord
-    if len(x) != 0:
-        plt.close()
-    coord = x
-    return coord
 
 def zone_file(config, VIDEO_TO_TREAT):
-    areas_list={}
+    areas_list = {}
 
-    for cage in range (1,config['nb_cage']+1):
-        current_cage=[]
+    for cage in range(1, 20):
+        current_cage = []
         for element in VIDEO_TO_TREAT:
-            if element[0]==str(cage):
+            if element[0] == str(cage):
                 current_cage.append(element)
-        
-        if current_cage!=[]:
+
+        if current_cage:
             for calibration in range(len(config['zone_name'])):
-                areas_list["Cage"+str(cage)+"_"+"Zone"+str(config['zone_name'][calibration])]=AreaSelector(calibration,cage,config, current_cage[0])
+                areas_list[f"Cage {cage}_Zone{config['zone_name'][calibration]}"] = AreaSelector(
+                    f"Please, define Zone {config['zone_name'][calibration]} for cage {cage}",
+                    f"{SCRIPT_PATH}/Datas/{element}")
 
     areas_list = json.dumps(areas_list)
 
-    zone_file = open(SCRIPT_PATH+"/zone.txt", "w")
+    zone_file = open(SCRIPT_PATH + "/zone.txt", "w")
     zone_file.write(str(areas_list))
     zone_file.close()
 
+
 if __name__ == "__main__":
     zone_file(config, VIDEO_TO_TREAT)
-
-
