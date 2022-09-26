895d9ce9487579d6f31e20b5c1d78ef4567418d0
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2022-09-26 10:42:22 -0400

    Create a function to crop in size and length a selected video press Q to save modification press C to crop video

--- DeepLodocus/videoCrop.py
+++ DeepLodocus/videoCrop.py
@@ -1,48 +1,25 @@
 import cv2
 import datetime
 import time
-from tkinter import Tk, Entry, Button, Label, filedialog
+from utils import AreaSelector, FileSelector, FileSaver
 
 
-def OutputName():
-    """
-    This function pop a window in which user will give a output name of the cropped video
-    :return:FileName -> Name of the output video
-    """
-
-    def saveFct():
-        FileName = str(e1.get())
-        if FileName != "":
-            master.quit()
-            return FileName
-
-    master = Tk()
-    Label(master, text="Output File Name").grid(row=0)
-    master.title("Save Your File")
-
-    e1 = Entry(master)
-    e1.grid(row=0, column=1)
-
-    b1 = Button(master,
-                text='Save',
-                command=saveFct)
-
-    b1.grid(row=0, column=2)
-
-    master.mainloop()
-
-    return saveFct()
-
-
-class videocropper():
+class videocropper:
     """
     Class of the video to be cropped.
     Using openCV, will determine few parameters (video length, format, FPS).
     User will use 2 slider to determine the beginning and end of the desired cropped video
-
     """
 
     def __init__(self, videopath):
+
+        self.video = cv2.VideoCapture(videopath)
+        self.video_extension = videopath.split(".")[-1]
+        self.video_length = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.video_width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.video_height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.FPS = int(self.video.get(cv2.CAP_PROP_FPS))
+        self.cropped = False
 
         def getFrame(frame_nr):
             """
@@ -50,13 +27,6 @@ class videocropper():
             :param frame_nr: timeframe
             """
             self.video.set(cv2.CAP_PROP_POS_FRAMES, frame_nr)
-
-        self.video = cv2.VideoCapture(videopath)
-
-        self.video_length = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.video_width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.video_height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.FPS = int(self.video.get(cv2.CAP_PROP_FPS))
 
         cv2.namedWindow("Video")
 
@@ -74,34 +44,45 @@ class videocropper():
 
             if ret:
                 cv2.imshow("Video", frame)
-
             else:
                 pass
 
             key = cv2.waitKey(self.FPS)
+            if key == ord('c'):
+
+                coord = AreaSelector("Crop your video", videopath)
+                x_list, y_list = [x_tuple[0] for x_tuple in coord], [y_tuple[1] for y_tuple in coord]
+                x_min, x_max, y_min, y_max = int(min(x_list)), int(max(x_list)), int(min(y_list)), int(max(y_list))
+
+                self.video_width = int(x_max - x_min)
+                self.video_height = int(y_max - y_min)
+                self.cropped = True
 
             if key == ord('q'):
-                outputname = OutputName()
-                start_time = time.time()
 
-                print("Start at {} (frame {})".format(str(datetime.timedelta(seconds=START_VIDEO/self.FPS)), START_VIDEO))
-                print("End at {} (frame {})".format(str(datetime.timedelta(seconds=END_VIDEO/self.FPS)), END_VIDEO))
+                print(f"Start at {str(datetime.timedelta(seconds=START_VIDEO / self.FPS))}")
+                print(f"End at {str(datetime.timedelta(seconds=END_VIDEO / self.FPS))}")
 
                 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-                writer = cv2.VideoWriter('{}.mp4'.format(outputname), fourcc, self.FPS,
+
+                writer = cv2.VideoWriter(FileSaver("Select where to save video", [("Video files", "."+self.video_extension)]), fourcc, self.FPS,
                                          (self.video_width, self.video_height))
 
+                start_time = time.time()
                 for i in range(START_VIDEO, END_VIDEO):
                     ret, frame = self.video.read()
                     if ret:
-                        if START_VIDEO < i:
+                        if START_VIDEO < i and self.cropped:
+                            crop_frame = frame[y_min:y_max, x_min:x_max]
+                            writer.write(crop_frame)
+                        elif START_VIDEO < i and not self.cropped:
                             writer.write(frame)
 
                 writer.release()
                 self.video.release()
+
                 cv2.destroyAllWindows()
-                print("Cropping excution lasted %s seconds" % (time.time() - start_time))
-                # release resources
+                print(f"Cropping execution lasted {(time.time() - start_time)} seconds")
                 break
 
 
@@ -110,9 +91,7 @@ if __name__ == "__main__":
     1. Open a finder window to select video to be cropped
     2. Crop video selected video by creating an instance of the 'videocropper' class for each of them
     """
-    root = Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilenames()
+    file_path = FileSelector("Select video(s) to edit", True, [("Video files", ".mp4 .MOV .avi")])
 
     for file in file_path:
         videocropper(file)
