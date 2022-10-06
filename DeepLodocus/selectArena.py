1db5ce6cb5666d86a6ea23e4442a844b870d3dcd
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2022-10-06 17:27:38 -0400

    updated dict serializer

--- DeepLodocus/selectArena.py
+++ DeepLodocus/selectArena.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+ matplotlib-3.3.2
 Created on Mon Apr 25 16:40:02 2022
 
 @author: maximeteixeira
@@ -9,12 +10,11 @@ import glob
 import json
 import os
 from pathlib import Path
-from utils import AreaSelector
+from newUtils import AreaSelector, dictSerializer
 
 SCRIPT_PATH = str(Path(__file__).parent)
 
-with open(SCRIPT_PATH + "/config.txt", "r") as config_file:
-    config = json.loads(config_file.read())
+config = dictSerializer.loadJSON(SCRIPT_PATH + '/config.txt')
 
 VIDEO_TO_TREAT = []
 for file in glob.glob(os.path.join(SCRIPT_PATH, 'Datas', "*.mp4")):
@@ -27,16 +27,13 @@ def ZoneSelector(config, VIDEO_TO_TREAT):
     areas_list = {}
 
     for element in VIDEO_TO_TREAT:
+        video = AreaSelector(f"{SCRIPT_PATH}/Datas/{element}")
         for calibration in range(len(config['zone_name'])):
-            areas_list[f"Cage{element[0]}_Zone{config['zone_name'][calibration]}"] = AreaSelector(
-                f"Please, define Zone {config['zone_name'][calibration]} for cage {element[0]}",
-                f"{SCRIPT_PATH}/Datas/{element}")
+            areas_list[f"Cage{element[0]}_Zone{config['zone_name'][calibration]}"] = video.polyselector(
+                f"Please, define Zone {config['zone_name'][calibration]} for cage {element[0]}")
 
-    areas_list = json.dumps(areas_list)
 
-    zone_file = open(SCRIPT_PATH + "/zone.txt", "w")
-    zone_file.write(str(areas_list))
-    zone_file.close()
+    dictSerializer.saveJSON(areas_list, SCRIPT_PATH + '/zone.txt')
 
 
 if __name__ == "__main__":
