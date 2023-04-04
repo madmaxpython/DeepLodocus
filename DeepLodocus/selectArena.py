7068f70ce2f0d316dc9d51ceaf73a588ab33add7
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2023-04-04 15:52:44 -0400

    Update
    Adapting the selectArea script for YAML files

--- DeepLodocus/selectArena.py
+++ DeepLodocus/selectArena.py
@@ -7,34 +7,72 @@ Created on Mon Apr 25 16:40:02 2022
 @author: maximeteixeira
 """
 import glob
-import json
+import yaml
 import os
 from pathlib import Path
-from newUtils import AreaSelector, DictSerializer
-
-SCRIPT_PATH = str(Path(__file__).parent)
-
-config = DictSerializer.loadJSON(SCRIPT_PATH + '/config.txt')
-
-VIDEO_TO_TREAT = []
-for file in glob.glob(os.path.join(SCRIPT_PATH, 'Datas', "*.mp4")):
-    VIDEO_TO_TREAT.append(file.split("/")[-1])
-
-VIDEO_TO_TREAT = sorted(VIDEO_TO_TREAT)
+from newUtils import AreaSelector, DictSerializer, YamlConfig
+import numpy as np
 
 
-def ZoneSelector(config, VIDEO_TO_TREAT):
+def ZoneSelector(config, VIDEO_PATH, VIDEO_TO_TREAT):
     areas_list = {}
-
     for element in VIDEO_TO_TREAT:
-        video = AreaSelector(f"{SCRIPT_PATH}/Datas/{element}")
-        for calibration in range(len(config['zone_name'])):
-            areas_list[f"Cage{element[0]}_Zone{config['zone_name'][calibration]}"] = video.polygon_selector(
-                f"Please, define Zone {config['zone_name'][calibration]} for cage {element[0]}")
+        areas_list[f"Cage{element[0]}"] = {}
+        # video = AreaSelector(f"{SCRIPT_PATH}/Datas/{element}")
+        for calibration in config['ZONE']['zone_name']:
+            areas_list[f"Cage{element[0]}"][calibration] = AreaSelector(
+                os.path.join(VIDEO_PATH, element)).polygon_selector(
+                f"Please, define Zone {calibration} for cage {element[0]}")
+    return areas_list
 
 
-    DictSerializer.saveJSON(areas_list, SCRIPT_PATH + '/zone.txt')
+def represent_scalar(dumper, data):
+    if isinstance(data, np.float64):
+        return dumper.represent_float(float(data))
+    return dumper.represent_scalar('tag:yaml.org,2002:str', str(data))
 
 
-if __name__ == "__main__":
-    ZoneSelector(config, VIDEO_TO_TREAT)
+def represent_array(dumper, data):
+    return dumper.represent_sequence('tag:yaml.org,2002:seq', data.tolist())
+
+
+def represent_ndarray(dumper, data):
+    return represent_array(dumper, data.flatten())
+
+
+def represent_tuple(dumper, data):
+    return dumper.represent_sequence('tag:yaml.org,2002:seq', list(data))
+
+
+yaml.add_representer(np.ndarray, represent_ndarray)
+yaml.add_representer(np.float64, represent_scalar)
+yaml.add_representer(tuple, represent_tuple)
+
+def Arena_selector(VIDEO_PATH, VIDEO_EXTENSION, CONFIG_PATH):
+    SCRIPT_PATH = str(Path(__file__).parent)
+
+    config_YAML = YamlConfig(CONFIG_PATH)
+
+    config = config_YAML.load()
+
+    VIDEO_TO_TREAT = []
+    for file in glob.glob(os.path.join(VIDEO_PATH, f"*{VIDEO_EXTENSION}")):
+        VIDEO_TO_TREAT.append(file.split("/")[-1])
+    print(VIDEO_TO_TREAT)
+    VIDEO_TO_TREAT = sorted(VIDEO_TO_TREAT)
+
+    sample_files = list()
+
+    for file_name in VIDEO_TO_TREAT:
+        file_number = file_name[0]
+        if file_number not in sample_files:
+            example_file = next(file for file in VIDEO_TO_TREAT if file.startswith(file_number))
+            sample_files.append(example_file)
+
+    area_coord = ZoneSelector(config, VIDEO_PATH, sample_files)
+
+    config['ZONE'] = {'zone_name': config['ZONE']['zone_name']}
+
+    config['ZONE'].update(area_coord)
+
+    config_YAML.save(config)
