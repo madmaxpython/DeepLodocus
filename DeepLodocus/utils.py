9317194e5d0f9ce06deb718d3b6d5e7e6391a6ff
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2023-04-04 17:14:36 -0400

    Updating yaml files

--- DeepLodocus/utils.py
+++ DeepLodocus/utils.py
@@ -12,56 +12,61 @@ from numpy import sqrt as sqrt
 import random
 from matplotlib.widgets import PolygonSelector
 from shapely.geometry import Point
-from tkinter import Tk, simpledialog, filedialog
+from tkinter import *
+from tkinter import messagebox, filedialog
 import cv2
 import json
+import yaml
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 
-def TotalDistance(config, data_reduced, likelihood):
+def total_distance(trackingData, likelihood,FPS, PIX_SIZE: float):
     """
     Measure the total distance travelled by the mice, using the 'spine1' label
-
     Parameters
     ----------
-    config: use config to know the pixel_size and the fps of the video #TODO
-    data_reduced: dataframe with coordinate of the mice without the 'likelihood' columns
-    likelihood: np.array with likelihood value
+    FPS: FPS of the original video
+    PIX_SIZE: size in cm of a pixel
+    trackingData: (np.array): tracking of the animal
+    likelihood: (np.array ): likelihood associate with tracking datas
 
-    Returns: Total distance travelled
+    Return:
     -------
+    tot_dist: Total distance travelled
     """
 
     temp_dist = []
-    chunk_size = int(config['fps_camera'] / 4)
+    chunk_size = int(FPS / 4)
     prev_x_y = 0
-    for i in range(0, len(data_reduced) - 10, chunk_size):
-        chunk = data_reduced['Mouse 1']['spine1'][i:i + chunk_size]
+    for i in range(0, len(trackingData) - 10, chunk_size):
+        chunk = trackingData[i:i + chunk_size]
 
         j = 0
         while j < len(chunk):
 
-            if likelihood[j + i][4] < 0.8:
+            if likelihood[j + i]:
                 j += 1
 
             else:
                 if prev_x_y == 0:
 
-                    prev_x_y = tuple(chunk.iloc[j])
+                    prev_x_y = tuple(chunk[j])
                 else:
-                    curr_x_y = tuple(chunk.iloc[j])
+                    curr_x_y = tuple(chunk[j])
                     distance = sqrt((prev_x_y[0] - curr_x_y[0]) ** 2 + (prev_x_y[1] - curr_x_y[1]) ** 2)
                     if distance < 200:
-                        temp_dist.append(distance * config['px_size'])
+                        temp_dist.append(distance * PIX_SIZE)
                     else:
                         temp_dist.append(0)
                     prev_x_y = curr_x_y
                 j += chunk_size + 1
 
-    tot_dist = np.nansum(temp_dist)
+    tot_dist = np.nansum(temp_dist)/PIX_SIZE
     return tot_dist
 
 
-def TotalTime(config, areas, data_reduced, likelihood, cage):
+def total_time(config, areas, trackingData, likelihood, cage):
     time_zone = {}
     entries_zone = {}
 
@@ -73,27 +78,27 @@ def TotalTime(config, areas, data_reduced, likelihood, cage):
 
     localization = ''  # variable of the localization (zone) of the animal
 
-    for frame in range(0, len(data_reduced) - 10, chunk_size):
-        chunk = data_reduced['Mouse 1'][:][frame:frame + chunk_size]  # take a chunk of the data
+    for frame in range(0, len(trackingData) - 10, chunk_size):
+        chunk = trackingData[frame:frame + chunk_size]  # take a chunk of the data
         bodypart = {}
         chunk_row = 0  # Counter through the chunk
 
         while chunk_row < len(chunk) and (
-                frame + chunk_row < (len(data_reduced) - 1)):  # execute while not at the end of chunk or dataset
+                frame + chunk_row < (len(trackingData) - 1)):  # execute while not at the end of chunk or dataset
 
-            it = iter(list(data_reduced.iloc[chunk_row + frame]))
-            pata = [*zip(it, it)]  # list of list of bodypart_coord
+            it = iter(list(trackingData.iloc[chunk_row + frame]))
+            current_BP = [*zip(it, it)]  # list of list of bodypart_coord
 
             for zone_name in config['zone_name']:  # fill bodypart dict with bodypart as key and 0 as value
                 bodypart[zone_name + '_bp'] = 0
 
-            for bodypart_coord in pata:
-                # print(frame+chunk_row, ' bodypart coordinate: ', pata , ' likelihood: ', likelihood[
-                # frame+chunk_row][pata.index(bodypart_coord)])
+            for bodypart_coord in current_BP:
+                # print(frame+chunk_row, ' bodypart coordinate: ', current_BP , ' likelihood: ', likelihood[
+                # frame+chunk_row][current_BP.index(bodypart_coord)])
                 for zone in config['zone_name']:
 
                     if areas['Cage' + cage + '_' + 'Zone' + zone].contains(Point(bodypart_coord)) and (
-                            likelihood[frame + chunk_row][pata.index(bodypart_coord)] > 0.8):
+                            likelihood[frame + chunk_row][current_BP.index(bodypart_coord)]):
 
                         bodypart[zone + '_bp'] += 1
 
@@ -104,7 +109,7 @@ def TotalTime(config, areas, data_reduced, likelihood, cage):
                                 entries_zone[zone] += 1
 
                     elif all(value == 0 for value in bodypart.values()) and chunk_row == len(chunk) and zone == \
-                            config['zone_name'][-1] and bodypart_coord == pata[-1]:
+                            config['zone_name'][-1] and bodypart_coord == current_BP[-1]:
                         localization = ''
                     elif chunk_row < len(chunk):
                         chunk_row += 1
@@ -117,59 +122,120 @@ def TotalTime(config, areas, data_reduced, likelihood, cage):
     return zone_dict
 
 
-def tkAskString(WINDOW_TITLE, GRID_TITLE):
-    file_path = simpledialog.askstring(WINDOW_TITLE, GRID_TITLE)
-    return file_path
+class AskInput:
+    def __init__(self, title, prompt, typeinput):
+        self.root = Tk()
+        self.root.title(title)
+        self.type_input = typeinput
 
-def FileSelector(TITLE, MULTIPLEFILES, FILETYPES):
+        self.prompt = Label(self.root, text=prompt)
+        self.prompt.grid(row=0, column=0)
+
+        self.entry = Entry(self.root)
+        self.entry.grid(row=1, column=0)
+
+        self.button = Button(self.root, text='Enter', command=self.get_entry)
+        self.button.grid(row=1, column=1)
+
+        self.root.mainloop()
+
+    def get_entry(self):
+        try:
+            value = self.convert(self.entry.get(), self.type_input)
+            self.root.quit()
+            return value
+        except:
+            messagebox.showwarning('Error', f'Entry is not a {self.type_input}')
+
+    @staticmethod
+    def convert(variable, type_asked):
+        return type_asked(variable)
+
+
+def file_selector(TITLE, MULTIPLE_FILES, FILETYPES):
     """
     Open a file dialog window to select files to transfer
     return: a list of files directory
     """
     selectwindow = Tk()
-    file_path = filedialog.askopenfilenames(title=TITLE, multiple = MULTIPLEFILES, filetypes=FILETYPES, parent=selectwindow)
-    selectwindow.mainloop()
+    selectwindow.withdraw()
+    file_path = list(filedialog.askopenfilenames(title=TITLE, multiple=MULTIPLE_FILES, filetypes=FILETYPES))
+    selectwindow.destroy()
+
     return file_path
 
-def FileSaver(TITLE, FILETYPES):
-    saver = Tk()
-    file_path = filedialog.asksaveasfilename(title=TITLE, filetypes=FILETYPES)
-    saver.quit()
+
+def file_saver(TITLE):
+    root = Tk()
+    root.withdraw()
+    file_path = filedialog.asksaveasfilename(title=TITLE)
     return file_path
-def AreaSelector(TITLE_WINDOW, VIDEO_PATH):
-    fig, ax = plt.subplots()
-    ax.invert_yaxis()
-    fig.canvas.manager.set_window_title('Calibration Step')
-    fig.suptitle(TITLE_WINDOW, fontsize=16)
 
-    cap = cv2.VideoCapture(VIDEO_PATH)
-    randomnb = random.randint(0, 300)
-    cap.set(1, randomnb)
 
-    ret, frame = cap.read()
-    imgplot = plt.imshow(frame)
+class AreaSelector:
+    def __init__(self, VIDEO_PATH: str):
+        self.VIDEO_PATH = VIDEO_PATH
+        self.lineprops = {'color': 'red', 'linewidth': 4, 'alpha': 0.8}
+        self.point = []
 
-    lineprops = {'color': 'red', 'linewidth': 4, 'alpha': 0.8}
-    lsso = PolygonSelector(ax=ax, onselect=onSelect, lineprops=lineprops)
-    plt.show()
-    return coord
+    def polygon_selector(self, TITLE_WINDOW):
+        self.fig, self.ax = plt.subplots()
+        self.ax.invert_yaxis()
+        self.fig.canvas.manager.set_window_title('Calibration Step')
 
-def onSelect(x):
-    global coord
-    if len(x) != 0:
-        plt.close()
-    coord = x
-    return coord
+        self.video = cv2.VideoCapture(self.VIDEO_PATH)
+        random_nb = random.randint(0, 300)
+        self.video.set(1, random_nb)
+        self.fig.suptitle(TITLE_WINDOW, fontsize=16)
 
-class dictSerializer:
+        ret, frame = self.video.read()
+        image_plot = plt.imshow(frame)
+        self.point = PolygonSelector(ax=self.ax, onselect=self.onClick, lineprops=self.lineprops)
+        plt.show()
+
+        return self.point
+
+    def onClick(self, event):
+        self.point = event
+        plt.close("all")
+        return self.point
+
+
+class DictSerializer:
     @staticmethod
     def saveJSON(config, path):
-        strconfig = json.dumps(config)
+        str_config = json.dumps(config)
         with open(path, 'w') as file:
-            file.write(strconfig)
+            file.write(str_config)
 
     @staticmethod
     def loadJSON(path):
-        with open(path, "r") as dictonary:
-            dictonary = json.loads(dictonary.read())
-        return dictonary
+        with open(path, "r") as dictionary:
+            dictionary = json.loads(dictionary.read())
+        return dictionary
+
+class YamlConfig:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        with open(self.filename, 'r') as f:
+            return yaml.safe_load(f)
+
+    def save(self, config_dict):
+        with open(self.filename, 'w') as f:
+            yaml.dump(config_dict, f)
+
+def it_imputer(data, tol=0.01, max_iter=10):
+    """
+    Very simple function to use the sklearn.impute.IterativeImputer
+    Parameters
+    ---------
+    data: np.array -> Data to be treated
+    tol: float -> tolerance threshold to be reached
+    max_iter: int -> max number of iterations to be executed
+    Returns
+    ---------
+    -> np.array : processed data by the sklearn.impute.IterativeImputer
+    """
+    return IterativeImputer(missing_values=np.nan, tol=tol, max_iter=max_iter).fit_transform(data)
